ba8fe4590f7f4b76168b38da7981595dec3a966b
sahako <sahako@gmail.com>
2013-07-02 18:33:11 +0900

    ~10.3.3

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -56,8 +56,6 @@ describe "Authentication" do
     describe "for non-signed-in users" do
       let(:user) { FactoryGirl.create(:user) }
 
-
-
       describe "when attempting to visit a protected page" do
         before do
           visit edit_user_path(user)
@@ -74,8 +72,6 @@ describe "Authentication" do
         end
       end
 
-
-
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -88,6 +84,21 @@ describe "Authentication" do
           specify { response.should redirect_to(signin_path) }
         end
       end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { response.should redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { response.should redirect_to(signin_path) }
+        end
+      end
+
+
     end
 
     describe "as wrong user" do
